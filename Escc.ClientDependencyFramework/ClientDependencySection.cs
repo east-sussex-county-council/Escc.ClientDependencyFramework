d5d43097b03c81ef63a8ab027b9daa5bd73d23fe
richardm <richard.mason@eastsussex.gov.uk>
2014-12-03 18:16:50 +0000

    Initial commit

---
+++ Escc.ClientDependencyFramework/ClientDependencySection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace Escc.ClientDependencyFramework
+{
+    public class ClientDependencySection : ClientDependency.Core.Config.ClientDependencySection
+    {
+        public new int Version
+        {
+            get
+            {
+                if (!String.IsNullOrEmpty(ConfigurationManager.AppSettings["ClientDependencyVersion"]))
+                {
+                    int appSettingsVersion;
+                    if (Int32.TryParse(ConfigurationManager.AppSettings["ClientDependencyVersion"], out appSettingsVersion))
+                    {
+                        return appSettingsVersion;
+                    }
+                }
+                return base.Version;
+            }
+            set { base.Version = value; }
+        }
+    }
+}
---
+++ Escc.ClientDependencyFramework/PolyfillMediaQueryRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ClientDependency.Core.FileRegistration.Providers;
+
+namespace Escc.ClientDependencyFramework
+{
+    public class PolyfillMediaQueryRenderer : StandardRenderer
+    {
+        protected override string RenderSingleCssFile(string css, IDictionary<string, string> htmlAttributes)
+        {
+            var html = base.RenderSingleCssFile(css, htmlAttributes);
+
+            htmlAttributes.Remove("media");
+            htmlAttributes["class"] = htmlAttributes["class"] + " mqIE";
+            html += "<!--[if (lte IE 8) & !(IEMobile 7) ]>" + base.RenderSingleCssFile(css, htmlAttributes) + "<![endif]-->";
+
+            return html;
+        }
+    }
+}
